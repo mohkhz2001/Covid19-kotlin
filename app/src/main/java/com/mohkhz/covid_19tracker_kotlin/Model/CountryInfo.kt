55cd2ca4c07bcd068ae648fa3ac8004275cd483b
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-20 21:29:48 +0430

    the model file changed => this is problem

--- app/src/main/java/com/mohkhz/covid_19tracker_kotlin/Model/CountryInfo.kt
+++ app/src/main/java/com/mohkhz/covid_19tracker_kotlin/Model/CountryInfo.kt
@@ -1,28 +1,10 @@
 package com.mohkhz.covid_19tracker_kotlin.Model
 
-
-data class CountryInfo(
-    val active: Int,
-    val activePerOneMillion: Double,
-    val cases: Int,
-    val casesPerOneMillion: Int,
-    val continent: String,
-    val country: String,
-    val countryInfo: CountryInfo,
-    val critical: Int,
-    val criticalPerOneMillion: Double,
-    val deaths: Int,
-    val deathsPerOneMillion: Double,
-    val oneCasePerPeople: Int,
-    val oneDeathPerPeople: Int,
-    val oneTestPerPeople: Int,
-    val population: Int,
-    val recovered: Int,
-    val recoveredPerOneMillion: Double,
-    val tests: Int,
-    val testsPerOneMillion: Int,
-    val todayCases: Int,
-    val todayDeaths: Int,
-    val todayRecovered: Int,
-    val updated: Long
+data class CountryData(
+    val _id: Int,
+    val flag: String,
+    val iso2: String,
+    val iso3: String,
+    val lat: Int,
+    val long: Int
 )
