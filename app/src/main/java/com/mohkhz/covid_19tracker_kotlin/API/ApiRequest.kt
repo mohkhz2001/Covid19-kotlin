9bdcb045b0734a3579276c749812b6ee6719422a
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-20 21:31:41 +0430

    the countries api changed --> imprt removed

--- app/src/main/java/com/mohkhz/covid_19tracker_kotlin/API/ApiRequest.kt
+++ app/src/main/java/com/mohkhz/covid_19tracker_kotlin/API/ApiRequest.kt
@@ -1,7 +1,6 @@
 package com.mohkhz.covid_19tracker_kotlin.API
 
 import com.mohkhz.covid_19tracker_kotlin.Model.CountriesInfoList
-import com.mohkhz.covid_19tracker_kotlin.Model.CountryInfo
 import com.mohkhz.covid_19tracker_kotlin.Model.Total
 import retrofit2.Call
 import retrofit2.http.GET
