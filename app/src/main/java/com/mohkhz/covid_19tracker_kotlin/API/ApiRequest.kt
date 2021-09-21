a9c9dd4f3777d8458d3c940374d21e8895ea9218
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-09-21 22:36:01 +0430

    the new api to get the history added

--- app/src/main/java/com/mohkhz/covid_19tracker_kotlin/API/ApiRequest.kt
+++ app/src/main/java/com/mohkhz/covid_19tracker_kotlin/API/ApiRequest.kt
@@ -1,9 +1,12 @@
 package com.mohkhz.covid_19tracker_kotlin.API
 
 import com.mohkhz.covid_19tracker_kotlin.Model.CountriesInfoList
+import com.mohkhz.covid_19tracker_kotlin.Model.CountryName
 import com.mohkhz.covid_19tracker_kotlin.Model.Total
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.ArrayList
 
 open interface ApiRequest {
 
@@ -12,5 +15,14 @@ open interface ApiRequest {
 
     @GET("countries")
     fun getCountries(): Call<CountriesInfoList>
+
+    @GET("{country}?lastdays={days}")
+    fun getHistory(
+        @Path("country") name: String,
+        @Path("days") days: Int
+    ): Call<CountriesInfoList>
+
+    @GET("historical?lastdays=1")
+    fun getHistoryCountry(): Call<ArrayList<CountryName>>
 
 }
